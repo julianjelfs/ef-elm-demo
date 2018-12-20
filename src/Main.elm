3ba14e39b6ce7a808603b5b14f37f762ea12f1bc
Julian Jelfs <julian.jelfs@gmail.com>
2018-12-20 22:30:05 +0000

    demo done

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,26 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
 
 
 
 ---- MODEL ----
 
 
-url : String
-url =
-    "api.openweathermap.org/data/2.5/weather?appid=" ++ apiKey ++ "&q="
+type alias WeatherReport =
+    { place : String
+    , weather : Maybe String
+    }
+
+
+weatherUrl : String
+weatherUrl =
+    "http://api.openweathermap.org/data/2.5/weather?appid=" ++ apiKey ++ "&q="
 
 
 apiKey : String
@@ -20,12 +29,20 @@ apiKey =
 
 
 type alias Model =
-    {}
+    { weatherReports : List WeatherReport
+    , searchTerm : Maybe String
+    , error : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { weatherReports = []
+      , searchTerm = Nothing
+      , error = False
+      }
+    , Cmd.none
+    )
 
 
 
@@ -34,11 +51,38 @@ init =
 
 type Msg
     = NoOp
+    | UpdateSearchTerm String
+    | RequestWeatherReport
+    | ReceivedWeatherReport (Result Http.Error WeatherReport)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        UpdateSearchTerm s ->
+            ( { model | searchTerm = Just s }
+            , Cmd.none
+            )
+
+        RequestWeatherReport ->
+            model.searchTerm
+                |> Maybe.map (\s -> ( model, getWeather s ))
+                |> Maybe.withDefault ( model, Cmd.none )
+
+        ReceivedWeatherReport (Ok weatherReport) ->
+            ( { model
+                | searchTerm = Nothing
+                , error = False
+                , weatherReports = weatherReport :: model.weatherReports
+              }
+            , Cmd.none
+            )
+
+        ReceivedWeatherReport (Err err) ->
+            ( { model | error = True }, Cmd.none )
 
 
 
@@ -48,9 +92,62 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Enter a city to see the weather" ]
+        , Html.form [ onSubmit RequestWeatherReport ]
+            [ input
+                [ autofocus True
+                , type_ "text"
+                , onInput UpdateSearchTerm
+                , value
+                    (Maybe.withDefault ""
+                        model.searchTerm
+                    )
+                ]
+                []
+            ]
+        , if model.error then
+            div [ class "error" ] [ text "Oh no something went wrong" ]
+
+          else
+            text ""
+        , div [] (weatherReports model.weatherReports)
         ]
+
+
+weatherReports : List WeatherReport -> List (Html Msg)
+weatherReports =
+    List.map
+        (\r ->
+            div
+                [ class "weather-report" ]
+                [ text (r.place ++ ": " ++ Maybe.withDefault "Unknown" r.weather)
+                ]
+        )
+
+
+
+---- API ------
+
+
+getWeather : String -> Cmd Msg
+getWeather s =
+    Http.send ReceivedWeatherReport <|
+        Http.get (weatherUrl ++ s) weatherReportDecoder
+
+
+weatherReportDecoder : Decode.Decoder WeatherReport
+weatherReportDecoder =
+    Decode.map2 WeatherReport
+        (Decode.field "name" Decode.string)
+        (Decode.map List.head
+            (Decode.field "weather"
+                (Decode.list
+                    (Decode.field "description"
+                        Decode.string
+                    )
+                )
+            )
+        )
 
 
 
